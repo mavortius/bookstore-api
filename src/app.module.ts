303ed4394a93c667dadcbf7e5f750d938207bdfa
mavortius <m.a.martins@live.com>
2021-10-04 17:56:04 -0300

    Created service layer

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductsController } from './products-controller';
+import { ProductService } from './product.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, ProductsController],
+  providers: [AppService, ProductService],
 })
 export class AppModule {}
